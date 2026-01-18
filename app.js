9c53e0f0a195e24cbd73a5d076d995cf0359438a
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-18 22:28:19 +0530

    change the name of script.js to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// GetElementById() - iske naam se pata chal raha hai hum
+// kisi bhi element ke ID se usko manipulate kar sakte hai
+
+const title = document.GetElementById('main-heading');
+
+console.log(title);
+
+//GetElementByClassName()
+
+const listItem = document.GetElementByClassName('list-items')
+
+console.log(listItem);
