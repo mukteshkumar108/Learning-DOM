5cf7ed409b5dcd531933e7371136927d1743a882
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-18 22:55:42 +0530

    added getElementByTagName(),querySelector() and // querySelectorAll() in app.js

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 // GetElementById() - iske naam se pata chal raha hai hum
 // kisi bhi element ke ID se usko manipulate kar sakte hai
 
-const title = document.GetElementById('main-heading');
+const title = document.getElementById('main-heading');
 
 console.log(title);
 
 //GetElementByClassName()
 
-const listItem = document.GetElementByClassName('list-items')
+// const listItem = document.GetElementsByClassName('list-items')
 
-console.log(listItem);
+// console.log(listItem);
+
+//getElementByTagName()
+
+const listItems = document.getElementsByTagName('li');
+
+console.log(listItems);
+
+//querySelector()
+
+// const container = document.querySelector('div');
+
+// console.log(container);
+
+// querySelectorAll()
+
+const container = document.querySelectorAll('div');
+
+console.log(container);
