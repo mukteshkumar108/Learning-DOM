fd3918feb90df989e7dfe3f7ef43f5858c174d1b
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-19 15:26:25 +0530

    added css made from AI to see the changes I make with js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // GetElementById() - iske naam se pata chal raha hai hum
 // kisi bhi element ke ID se usko manipulate kar sakte hai
 
-const title = document.getElementById('main-heading');
+// const title = document.getElementById('main-heading');
 
-console.log(title);
+// console.log(title);
 
 //GetElementByClassName()
 
@@ -13,9 +13,9 @@ console.log(title);
 
 //getElementByTagName()
 
-const listItems = document.getElementsByTagName('li');
+// const listItems = document.getElementsByTagName('li');
 
-console.log(listItems);
+// console.log(listItems);
 
 //querySelector()
 
@@ -25,6 +25,41 @@ console.log(listItems);
 
 // querySelectorAll()
 
-const container = document.querySelectorAll('div');
+// const container = document.querySelectorAll('div');
 
-console.log(container);
+// console.log(container);
+
+// const title = document.querySelector('#main-heading');
+
+// title.style.color = 'red';
+
+// console.log(title);
+
+// const listItems = document.querySelectorAll('list-items');
+
+// for(i = 0; i < listItems.lenght; i++){
+//     listItems[i].style.fontSize = '5rem';
+// }
+
+// console.log(listItems);
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+//Adding Elements
+
+ul.append(li)
+
+//Modifying the text
+li.innerText = 'X-men';
+
+//Modifying Attributes & Classes
+
+// li.setAttribute('id','main-heading');
+// li.removeAttribute('id');
+
+// const title = document.querySelector('#main-heading');
+
+// console.log(title.getAttribute('id'));
+
+li.classList.add('list-items')
